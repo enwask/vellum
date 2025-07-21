da881f3f134869099df8b337119e29cd76ab61f7
enwask <enwask@ucf.edu>
2025-07-21 18:01:15 +0100

    Revamp layout parsing

--- src/vellum/retrieval/layout.py
+++ src/vellum/retrieval/layout.py
@@ -11,12 +11,12 @@ from layoutparser.models.detectron2 import Detectron2LayoutModel, catalog
 from PIL.Image import Image
 import requests
 
-from vellum.utils import config
+from vellum.utils.configuration import config, LayoutParser
 
 
 # https://github.com/pradhanhitesh/LayoutParser-Install?tab=readme-ov-file#11-downloading-detectron2-via-layout-parser  # noqa
-def load_model() -> Detectron2LayoutModel:
-    config_path = f'{config.layout_parser_model}/config'
+def load_model(parser: LayoutParser) -> Detectron2LayoutModel:
+    config_path = f'{parser.model}/config'
 
     config_path_split = config_path.split('/')
     dataset_name = config_path_split[-3]
@@ -27,7 +27,7 @@ def load_model() -> Detectron2LayoutModel:
     model_url = catalog.MODEL_CATALOG[dataset_name][model_name]
     config_url = catalog.CONFIG_CATALOG[dataset_name][model_name]
 
-    model_path = Path('.vellum') / 'layout_model'
+    model_path = Path('.vellum') / 'layout_models' / dataset_name / model_name
     model_path.mkdir(parents=True, exist_ok=True)
 
     config_file_path, model_file_path = None, None
@@ -61,12 +61,16 @@ def load_model() -> Detectron2LayoutModel:
     return Detectron2LayoutModel(
         config_path=str(config_file_path),
         model_path=str(model_file_path),
-        extra_config=config.layout_parser_config,
-        label_map=config.layout_parser_label_map,
+        extra_config=parser.config,
+        label_map=parser.label_map,
     )
 
 
-layout_model = load_model()
+# Load all layout models defined in the configuration
+layout_models = [
+    load_model(parser)
+    for parser in config.layout_parsers
+]
 
 
 class LayoutElement(NamedTuple):
@@ -79,34 +83,74 @@ class LayoutElement(NamedTuple):
     y2: float
     type: str
 
+    def size(self) -> float:
+        """
+        Returns the area of the element.
+        """
+        return (self.x2 - self.x1) * (self.y2 - self.y1)
+
+    def contains(self, other: 'LayoutElement', epsilon: float) -> bool:
+        """
+        Returns True if the other element is within this element's bounding
+        box, padded by the specified amount.
+        """
+        return (self.x1 - epsilon <= other.x1 <= self.x2 + epsilon and
+                self.y1 - epsilon <= other.y1 <= self.y2 + epsilon and
+                self.x1 - epsilon <= other.x2 <= self.x2 + epsilon and
+                self.y1 - epsilon <= other.y2 <= self.y2 + epsilon)
+
+
+def remove_contained(elements: list[LayoutElement],
+                     epsilon: float) -> list[LayoutElement]:
+    """
+    Remove elements that are completely contained by another element.
+    """
+    elements = sorted(elements, key=LayoutElement.size, reverse=True)
+    res: list[LayoutElement] = []
+    for element in elements:
+        if not any(other.contains(element, epsilon=epsilon)
+                   for other in res):
+            res.append(element)
+
+    return res
+
 
 def parse_layout(image: Image,
-                 x_padding: float = 18,
-                 y_padding: float = 8) -> list[LayoutElement]:
+                 x_padding: float = 20,
+                 y_padding: float = 20,
+                 containment_epsilon: float = 60.0) -> list[LayoutElement]:
     cv2_image = np.array(image.convert('RGB'))
     cv2_image = cv2_image[:, :, ::-1].copy()  # BGR for OpenCV
 
-    layout = layout_model.detect(cv2_image)
+    # Combine layouts from all models
+    layouts = [model.detect(cv2_image) for model in layout_models]
     elements: list[LayoutElement] = []
 
-    for block in layout:
-        if TYPE_CHECKING:
-            assert isinstance(block, TextBlock)
+    for layout in layouts:
+        for block in layout:
+            if TYPE_CHECKING:
+                assert isinstance(block, TextBlock)
 
-        rect: Rectangle = block.block  # type: ignore
-        type: str = block.type # type: ignore
+            rect: Rectangle = block.block  # type: ignore
+            type: str | None = block.type
 
-        # We pad the coordinates a little bit, clamping to the image size and rounding
-        x1 = int(max(0, rect.x_1 - x_padding))
-        y1 = int(max(0, rect.y_1 - y_padding))
-        x2 = int(min(image.width, rect.x_2 + x_padding))
-        y2 = int(min(image.height, rect.y_2 + y_padding))
-        elements.append(LayoutElement(
-            x1=x1,
-            y1=y1,
-            x2=x2,
-            y2=y2,
-            type=type,
-        ))
+            if type is None:
+                # Key omitted from the label map (or mapped to None); skip it
+                continue
 
+            # We pad the coordinates a little bit, clamping to the image size and rounding
+            x1 = int(max(0, rect.x_1 - x_padding))
+            y1 = int(max(0, rect.y_1 - y_padding))
+            x2 = int(min(image.width, rect.x_2 + x_padding))
+            y2 = int(min(image.height, rect.y_2 + y_padding))
+            elements.append(LayoutElement(
+                x1=x1,
+                y1=y1,
+                x2=x2,
+                y2=y2,
+                type=type,
+            ))
+
+    # Remove elements that are completely contained by another element
+    elements = remove_contained(elements, epsilon=containment_epsilon)
     return elements
--- src/vellum/utils/configuration.py
+++ src/vellum/utils/configuration.py
@@ -1,3 +1,21 @@
+from functools import cached_property
+from typing import NamedTuple
+
+
+class LayoutParser(NamedTuple):
+    """
+    Describes a layout parser configuration. Multiple configurations mayb
+    be specified to combine different models or settings.
+    """
+    model: str
+    label_map: dict[int, str | None]
+    config: list[str | float]
+
+    def __str__(self) -> str:
+        return f"LayoutParser(model={self.model}, labels={self.label_map})"
+
+
+
 class Config:
     # Placeholder for possible dynamic configuration in the future
     @property
@@ -13,29 +31,57 @@ class Config:
         # return 'qwen3:32b'
         return 'llama4:latest'
 
-    @property
-    def layout_parser_model(self) -> str:
-        return 'lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x'
-
-    @property
-    def layout_parser_label_map(self) -> dict[int, str]:
-        return {
-            0: 'text',
-            1: 'title',
-            2: 'list',
-            3: 'table',
-            4: 'figure',
-        }
-
-    @property
-    def layout_parser_config(self) -> list[str | float]:
+    @cached_property
+    def layout_parsers(self) -> list[LayoutParser]:
+        """
+        Returns a list of layout parser configurations.
+        """
         return [
-            'MODEL.ROI_HEADS.SCORE_THRESH_TEST', .75,
+            # PubLayNet text + figures (low threshold)
+            LayoutParser(
+                model='lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x',
+                label_map={
+                    0: 'text',
+                    1: None,
+                    2: None,
+                    3: 'table',
+                    4: 'figure',
+                },
+                config=[
+                    'MODEL.ROI_HEADS.SCORE_THRESH_TEST', .3,
+                ],
+            ),
+
+            # PubLayNet lists + titles (higher threshold)
+            LayoutParser(
+                model='lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x',
+                label_map={
+                    0: None,
+                    1: 'title',
+                    2: 'list',
+                    3: None,
+                    4: None,
+                },
+                config=[
+                    'MODEL.ROI_HEADS.SCORE_THRESH_TEST', .6,
+                ],
+            ),
+
+            # MFD (math formulas)
+            LayoutParser(
+                model='lp://MFD/faster_rcnn_R_50_FPN_3x',
+                label_map={
+                    1: 'math',
+                },
+                config=[
+                    'MODEL.ROI_HEADS.SCORE_THRESH_TEST', .4,
+                ],
+            )
         ]
 
     @property
     def data_version(self) -> int:
         # Versioning for parsed document metadata
-        return 7
+        return 28
 
 config = Config()
